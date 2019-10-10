d4f029f15a3c5f3992d48ce4453eb4e672fa2b6e
rcrdlbl <bluh@outlook.com>
2019-10-10 15:38:42 -0400

    installed dependencies, set up routing

---
+++ src/containers/AllExpenses.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class AllExpenses extends Component {
+  render() {
+    return(
+      <h1>All Expenses List In A Few Min</h1>
+    )
+  }
+}
+
+export default AllExpenses
